86df0640b7854c0e150cffc8ed17c67299c798af
Narendra Santhosh Nagarajan <santhoshnarendra@gmail.com>
2021-04-20 15:23:24 +0530

    wip: completed JS Dom

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var phoneNumber = 8056160364;
 var isGoodBoy = undefined;
 var accountBalance = null;
 console.log(typeof personName); // string
-personName= 987;
+personName = 987;
 var personCharacterstics = [{
     hair: "black",
     gender: "male",
@@ -29,10 +29,10 @@ console.log(typeof accountBalance);
 console.log(typeof personCharacterstics);
 
 // let scenario
-let test ="test1"
+let test = "test1"
 
-function runFunc () {
-     let test="rest1"
+function runFunc() {
+    let test = "rest1"
 
     // console.log("rest");
     console.log(test); // rest1
@@ -47,3 +47,19 @@ runFunc();
 console.log(test);
 
 // runFunc1();
+
+
+const sendMessage = () => {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phoneNumber = document.getElementById("number").value;
+    const message = document.getElementById("message").value;
+    //  string literals in javascript
+    const obj = {
+        name,
+        email,
+        phoneNumber,
+        message
+    };
+    console.log(obj);
+}
